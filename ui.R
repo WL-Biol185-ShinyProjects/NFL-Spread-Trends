60290e75f50af9fe5fd63ea8f898b02856453487
Christian Agreda <agredac23@mail.wlu.edu>
2022-10-24 16:09:09 -0400

    new ui

--- ui.R
+++ ui.R
@@ -1,24 +1,23 @@
-library(shiny)
-
-# Define UI for application that draws a histogram
-fluidPage(
+  library(shiny)
+  library(dplyr)
+  library(ggplot2)
   
-  # Application title
-  titlePanel("NFL Favorite vs Spread
-             "),
-  
-  
-  #Update SelectInput Dynamically
-  observe({
-    updateSelectInput(session, "sel_NFL_Team", choices = spread_df$team1)
-  })
-  
+  server <- function(input, output, session) {
+    
+    sales = read.csv("NFL_Spread_Data.csv", header = TRUE,  sep = ",")
+    }
+    
+    #Update SelectInput Dynamically
+    observe({
+      updateSelectInput(session,"sel_NFL_Team", choices = spread_df$team1)
+    })
+    
   
   ui <- basicPage(
-    h1("NFL team"),
-    selectInput(inputId = "sel_NFL_Team",
-                label = "Choose NFL Team",
-                "Names")
-  )
+      h1("NFL team"),
+      selectInput(inputId = "sel_NFL_Team",
+                  label = "Choose NFL Team",
+                  "Names")
+    )
   
   shinyApp(ui = ui, server = server)
