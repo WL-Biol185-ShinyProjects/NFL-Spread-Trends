99445ad33ccd1d5962bfb73e798941cec3c0af02
Christian Agreda <agredac23@mail.wlu.edu>
2022-10-26 16:15:36 -0400

    new

--- ui.R
+++ ui.R
@@ -12,7 +12,7 @@
                   choices = c("<-12", "-12 =< x < -9", "-9 =< x < -6","-6 =< x < -3", "-3 =< x < 0", "0", "0 < x < 3", "3 =< x < 6", "6 =< x < 9", "9 =< x =< 12", ">12"))
       textOutput("win_loss")
       
-    )
+    )g
   
   
   shinyApp(ui = ui, server = server)
