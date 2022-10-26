844d5a80b57c113a73c070f0dcc2886887617bf2
Christian Agreda <agredac23@mail.wlu.edu>
2022-10-26 16:14:27 -0400

    fix

--- ui.R
+++ ui.R
@@ -9,7 +9,7 @@
                   choices = list(unique(df$team))),
       selectInput("sel_spread",
                   label = "Choose a spread",
-                  choices = c = ("<-12", "-12 =< x < -9", "-9 =< x < -6","-6 =< x < -3", "-3 =< x < 0", "0", "0 < x < 3", "3 =< x < 6", "6 =< x < 9", "9 =< x =< 12", ">12")
+                  choices = c("<-12", "-12 =< x < -9", "-9 =< x < -6","-6 =< x < -3", "-3 =< x < 0", "0", "0 < x < 3", "3 =< x < 6", "6 =< x < 9", "9 =< x =< 12", ">12"))
       textOutput("win_loss")
       
     )
