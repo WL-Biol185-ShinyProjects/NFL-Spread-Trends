8f94309766daf15a621456cd35d31c83fd9cfe33
Christian Agreda <agredac23@mail.wlu.edu>
2022-10-31 15:49:39 -0400

    merge

--- server.R
+++ server.R
@@ -1,16 +1,22 @@
 library(shiny)
+library(ggplot2)
 
-# Define server logic required to draw a histogram
-function(input, output) {
+tidy_df = read.csv('tidy_df.csv')
+
+# Define server logic for drop down memu
+server = function(input, output) {
   
-  output$distPlot <- renderPlot({
+  win = tidy_df["team" == input$sel_team & tidy_df$diff > 0]
+  
+  output$win_loss = renderText({"The", input$sel_team, "is" 
     
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
     
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+  })
+    
+  })
+  
+  output$selected_team = renderText({
+    paste("Select the team you would like to find out about:")
     
   })
   
