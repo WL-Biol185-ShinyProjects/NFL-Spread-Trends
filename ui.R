f810373b9dd8aeedb29dfe21a520632a181bfe8e
Christian Agreda <agredac23@mail.wlu.edu>
2022-10-31 14:50:50 -0400

    website debugging

--- ui.R
+++ ui.R
@@ -2,17 +2,19 @@
   library(dplyr)
   library(ggplot2)
   
+  server = function(input, output, session) {}
+  
     ui <- fluidPage(
       titlePanel("NFL team spead odds"),
       selectInput("sel_team",
                   label = "Choose an NFL Team",
-                  choices = list(unique(df$team))),
+                  choices = c("c", "a", "d")),
       selectInput("sel_spread",
                   label = "Choose a spread",
-                  choices = c("<-12", "-12 =< x < -9", "-9 =< x < -6","-6 =< x < -3", "-3 =< x < 0", "0", "0 < x < 3", "3 =< x < 6", "6 =< x < 9", "9 =< x =< 12", ">12"))
+                  choices = c("<-12", "-12 =< x < -9", "-9 =< x < -6","-6 =< x < -3", "-3 =< x < 0", "0", "0 < x < 3", "3 =< x < 6", "6 =< x < 9", "9 =< x =< 12", ">12")),
       textOutput("win_loss")
       
-    )g
+    )
   
   
   shinyApp(ui = ui, server = server)
