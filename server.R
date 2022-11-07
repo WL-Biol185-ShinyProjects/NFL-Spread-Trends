8791c6756cecd5538ce33bd6b8f995500365f990
Will Catalano <catalanow23@mail.wlu.edu>
2022-11-07 17:13:49 -0500

    created sidebar, got app running, added blank graph

--- server.R
+++ server.R
@@ -1,15 +1,29 @@
 library(shiny)
 library(ggplot2)
+library(usmap)
 
-tidy_df = read.csv("tidy_df.csv")
-    
+df = read.csv("tidy_df.csv")
+
 #Define server logic fro drop down menu
-server = function(input, output) {  
-  win = tidy_df["team" == input$sel_team & tidy_df$diff >0]
-  output$win_loss = renderText({"The" + input$sel_spread + "is"
-    })
-  }
-
-output$selected_team = renderText({
-  paste("Select the team you would like to find out about")
-})
+function(input, output, server) {  
+  
+  output$win_loss = renderText({ 
+    
+    selected_df_spread = filter(df, spread >= input$sel_spread[1], spread <= input$sel_spread[2])
+    
+    selected_df_team = filter(selected_df_spread, team == input$sel_team)
+    
+    
+    win = sum(selected_df_team['difference'] > 0)
+    loss = sum(selected_df_team['difference'] < 0)
+    tie = sum(selected_df_team['difference'] == 0)
+    
+    paste('The ' , input$sel_team, " are ", win, "-", loss, "-", tie, "against spreads between ", input$sel_spread[1], " and ", input$sel_spread[2], " points.")
+    
+  })
+  
+  output$heat_map = renderPlot({ plot_usmap()
+    
+    
+  })
+}
--- ui.R
+++ ui.R
@@ -1,22 +1,30 @@
   library(shiny)
   library(dplyr)
   library(ggplot2)
-
-  server <- function(input, output, session) {}
+  library(usmap)
+  
   df = read.csv("tidy_df.csv")
   
-    ui <- fluidPage(
-      titlePanel("NFL team spead odds"),
-      selectInput("sel_team",
-                  label = "Choose an NFL Team",
-                  choices = c("Arizona Cardinals", "Atlanta Falcons", "Baltimore Ravens", "Buffalo Bills", "Carolina Panthers", "Chicago Bears", "Cincinnati Bengals", "Cleveland Browns", "Dallas Cowboys", "Denver Broncos", "Detroit Lions", "Green Bay Packers", "Houston Texans", "Indianapolis Colts", "Jacksonville Jaguars", "Kansas City Chiefs", "Las Vegas Raiders", "Los Angeles Chargers", "Los Angeles Rams", "Miami Dolphins", "Minnesota Vikings", "New England Patriots", "New Orleans Saints", "New York Giants", "New York Jets", "Philadelphia Eagles", "Pittsburgh Steelers", "San Francisco 49ers", "Seattle Seahawks", "Tampa Bay Buccaneers", "Tennessee Titans", "Washington Commanders")),
-      selectInput("sel_spread",
-                  label = "Choose a spread",
-                  choices = c("<-12", "-12 =< x < -9", "-9 =< x < -6","-6 =< x < -3", "-3 =< x < 0", "0", "x = 0 < x < 3", "3 =< x < 6", "6 =< x < 9", "9 =< x =< 12", ">12")),
-      textOutput("win_loss")
+    fluidPage(
+      titlePanel("How good is each NFL team against the spread since 2000?"),
+      
+      sidebarLayout(
+        sidebarPanel(
+          selectInput("sel_team",
+                      label = "Choose an NFL Team",
+                      choices = unique(df['team'])),
+          
+          sliderInput("sel_spread",
+                      label = "Choose a spread",
+                      min = min(df['spread']),
+                      max = max(df['spread']),
+                      value = c(-5,5)),
+          
+          textOutput("win_loss")),
+
+        mainPanel(plotOutput('heat_map'))
+        
+      )
+
       
     )
-
-
-
-  shinyApp(ui = ui, server = server)
